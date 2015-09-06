4c034bfe93232d2adc2fe7e515e11d433329185d
szymzet <sz.zabkiewicz@gmail.com>
2015-09-06 16:22:38 +0200

    Improved mark popping from github.com/magnars/.emacs.d

--- personal/20-emacs-settings.el
+++ personal/20-emacs-settings.el
@@ -30,6 +30,17 @@
 (setq display-time-day-and-date nil)
 (display-time)
 
+;; When popping the mark, continue popping until the cursor actually moves
+;; Also, if the last command was a copy - skip past all the expand-region cruft.
+(defadvice pop-to-mark-command (around ensure-new-position activate)
+  (let ((p (point)))
+    (when (eq last-command 'save-region-or-current-line)
+      ad-do-it
+      ad-do-it
+      ad-do-it)
+    (dotimes (i 10)
+      (when (= p (point)) ad-do-it))))
+
 ;; customize
 (custom-set-faces
  '(aw-leading-char-face
