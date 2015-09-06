b96455c700a91771a8265173dba7d6809a508dbf
szymzet <sz.zabkiewicz@gmail.com>
2015-09-06 16:23:48 +0200

    Full screen vc annotate from github.com/magnars/.emacs.d

--- personal/30-my-funs.el
+++ personal/30-my-funs.el
@@ -29,3 +29,20 @@
   (magit-status)
   (unless prefix
     (delete-other-windows)))
+
+;; full screen vc-annotate
+
+(defun vc-annotate-quit ()
+  "Restores the previous window configuration and kills the vc-annotate buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :vc-annotate-fullscreen))
+
+(eval-after-load "vc-annotate"
+  '(progn
+     (defadvice vc-annotate (around fullscreen activate)
+       (window-configuration-to-register :vc-annotate-fullscreen)
+       ad-do-it
+       (delete-other-windows))
+
+     (define-key vc-annotate-mode-map (kbd "q") 'vc-annotate-quit)))
