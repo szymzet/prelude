b5aeff1940b7a0e7b8b7a8e438bf19781283b3e4
szymzet <sz.zabkiewicz@gmail.com>
2015-09-26 15:38:49 +0200

    Movement hydra - no need for control

---
+++ personal/60-hydras.el
@@ -0,0 +1,31 @@
+(global-set-key
+ (kbd "C-n")
+ (defhydra hydra-move-start-witn-next
+   (:body-pre (next-line))
+   "move"
+   ("n" next-line)
+   ("p" previous-line)
+   ("f" forward-char)
+   ("b" backward-char)
+   ("a" beginning-of-line)
+   ("e" move-end-of-line)
+   ("v" scroll-up-command)
+   ;; Converting M-v to V here by analogy.
+   ("V" scroll-down-command)
+   ("l" recenter-top-bottom)))
+
+(global-set-key
+ (kbd "C-p")
+ (defhydra hydra-move-start-with-prev
+   (:body-pre (previous-line))
+   "move"
+   ("n" next-line)
+   ("p" previous-line)
+   ("f" forward-char)
+   ("b" backward-char)
+   ("a" beginning-of-line)
+   ("e" move-end-of-line)
+   ("v" scroll-up-command)
+   ;; Converting M-v to V here by analogy.
+   ("V" scroll-down-command)
+   ("l" recenter-top-bottom)))
